6b8c6819f4b0e110fe118198f24a3b1603f89e8d
imranpopz <imranpopz32@gmail.com>
2026-08-02 11:15:01 +0200

    sm6150-common: drop lineage livedisplay

--- sm6150.mk
+++ sm6150.mk
@@ -210,10 +210,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.xiaomi_sm6150
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
