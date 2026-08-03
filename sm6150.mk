e93490334b766226224d07b2002353b340170788
imranpopz <imranpopz32@gmail.com>
2026-08-03 13:29:40 +0200

    Revert "sm6150-common: Switch to android.hardware.usb@1.3-service.dual_role_usb" This reverts commit f531fce60d5f6b5507d9de4ae47a011a31497ce9.

--- sm6150.mk
+++ sm6150.mk
@@ -319,13 +319,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Uevent
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb@1.0-service \
     android.hardware.usb.gadget-service.qti \
     usb_compositions.conf
 
