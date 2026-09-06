7d90ec6e7b783f4d4b4ae2578c81f2eec2bf908a
Gagan Malvi <malvigagan@gmail.com>
2026-09-06 20:44:55 +0200

    sm6150-common: Move permissions to common. Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -229,10 +229,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     telephony
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
-
 # RIL
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay
